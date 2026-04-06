d5ca13ab567ef5ce3249c0a50878fc9250d941d6
wendelmjesus <wendelmjesus@outlook.comn>
2026-04-05 22:42:22 -0300

    refactor: various code improvements.

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
+
 const form = document.querySelector('.login-form');
 
-form.addEventListener('mousemove', (e) => {
-    const rect = form.getBoundingClientRect();
-    
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
+if (form) {
+    form.addEventListener('mousemove', (e) => {
+        const rect = form.getBoundingClientRect();
+        
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
 
-    form.style.setProperty('--x', `${x}px`);
-    form.style.setProperty('--y', `${y}px`);
-});
+        form.style.setProperty('--x', `${x}px`);
+        form.style.setProperty('--y', `${y}px`);
+    });
+}
 
 function toggleMenu() {
     const sidebar = document.getElementById('sidebar');
     const overlay = document.getElementById('overlay');
     const btn = document.querySelector('.menu-btn');
     
-    sidebar.classList.toggle('active');
-    overlay.classList.toggle('active');
-    btn.classList.toggle('active-btn'); 
+    if (sidebar && overlay && btn) {
+        sidebar.classList.toggle('active');
+        overlay.classList.toggle('active');
+        btn.classList.toggle('active-btn');
+    }
 }
 
+const header = document.querySelector(".main-header");
+
+if (header) {
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 50) {
+            header.classList.add("scrolled");
+        } else {
+            header.classList.remove("scrolled");
+        }
+    });
+}
+
+window.addEventListener("scroll", function () {
+    const header = document.querySelector(".main-header");
+
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
